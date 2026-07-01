336c3f57c60ecf156dc1196c55bf15254aea524c
Vicky Kumar <vickykumarpathaldiha7@gmail.com>
2026-07-01 20:17:48 +0530

    completed placeorder controller and frontend placeorder page

--- Server/Routes/order.routes.js
+++ Server/Routes/order.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { placeOrder } from "../Controllers/Order.controllers.js";
 import { isAuth } from "../Middlewares/isAuth.middlewares.js";
-export const orderRouter=express.Router();
+ const orderRouter=express.Router();
 
 orderRouter.post("/placeorder",isAuth,placeOrder)
+
+export default orderRouter;
